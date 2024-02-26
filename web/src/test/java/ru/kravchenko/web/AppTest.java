d3823cef6ca52b18dcc6218a9423ee5dc99b0dca
Кравченко Роман Владимирович (4061571) <rvkravchenko@vtb.ru>
2024-02-26 10:25:47 +0300

    first commit

---
+++ web/src/test/java/ru/kravchenko/web/AppTest.java
@@ -0,0 +1,13 @@
+package ru.kravchenko.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
